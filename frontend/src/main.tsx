a6ad34a89865ae6ce1ae841929277440a932b788
pabbasaiharsha <53151583+pabbasaiharsha@users.noreply.github.com>
2026-04-03 00:01:51 +0530

    hide the google client id properly

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <GoogleOAuthProvider clientId={import.meta.env.client_id}>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <App />
   </GoogleOAuthProvider>
 )
